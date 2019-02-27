86f3676878da5554d1b4b6c03fb5548b5411a6a1
Frederick Lau <32381290+fredwlau@users.noreply.github.com>
2019-02-27 17:32:09 -0500

    Completed Repeated A Star

--- RepeatedAStar.java
+++ RepeatedAStar.java
@@ -1,12 +1,16 @@
-
 import java.util.Collections;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-public class RepeatedAStar extends Maze {
-
+public class RepeatedAStar extends Maze implements Serializable {
+	
 	public RepeatedAStar(int row, int col, int start_row, int start_col, int goal_row, int goal_col) {
 		super(row, col, start_row, start_col, goal_row, goal_col);
 		// TODO Auto-generated constructor stub
@@ -72,30 +76,83 @@ public class RepeatedAStar extends Maze {
 		return is_path;	
 	}
 	
-	public static void main(String[] args) throws IOException {
+	//main method for creating and storing the mazes
+	
+	/*public static void main(String[] args) throws IOException {
 		boolean path;
-		int num = 0;
 		
 		RepeatedAStar new_search = new RepeatedAStar(101, 101, 0, 0, 100, 100);
-		
-	
-			
+
 			path = new_search.forward_backward(0, 0);
 			//forward
 			new_search.print_path(new_search.best_path);
 			if(path) {
-				String buffer = new_search.output2();
+				//successful maze with path from 0,0 to 100,100
+				//save the maze
+				try {
+					FileOutputStream file_out = new FileOutputStream("./maze50.ser");
+					ObjectOutputStream object_out = new ObjectOutputStream(file_out);
+					object_out.writeObject(new_search);
+					object_out.close();
+				}
+				catch(Exception ex) {
+					ex.printStackTrace();
+				}
+				String buffer = new_search.output_maze();
 				BufferedWriter file = new BufferedWriter(new FileWriter(new File("repeated.txt")));
 				file.write(buffer.toString());
 				file.flush();
 				file.close();
+				
 				new_search.clear_path();
-				//backward
 				new_search.forward_backward(1, 2);
 				new_search.print_path(new_search.best_path);
+				
+				try {
+					FileInputStream file_in = new FileInputStream("maze50.ser");
+					ObjectInputStream object_in = new ObjectInputStream(file_in);
+					RepeatedAStar m = (RepeatedAStar) object_in.readObject();
+					System.out.print(m.output_maze());
+					object_in.close();
+				}
+				catch(Exception ex) {
+					ex.printStackTrace();
+				}
 			
-			
-			num++;
+		}
+	}*/
+	
+	//main method for running pathfinding on the mazes
+	
+	public static void main(String [] args) {
+		
+		try {
+			FileInputStream file_in = new FileInputStream("maze1.ser");
+			ObjectInputStream object_in = new ObjectInputStream(file_in);
+			RepeatedAStar m = (RepeatedAStar) object_in.readObject();
+			m.clear_path();
+			boolean path = m.forward_backward(0, 0);
+			if(path) {
+				String buffer = m.output_maze();
+				BufferedWriter file = new BufferedWriter(new FileWriter(new File("maze1-forward-path.txt")));
+				file.write(buffer.toString());
+				file.flush();
+				file.close();
+				
+				
+				m.clear_path();
+				m.forward_backward(1, 2);
+				
+				buffer = m.output_maze();
+				file = new BufferedWriter(new FileWriter(new File("maze1-backward-path.txt")));
+				file.write(buffer.toString());
+				file.flush();
+				file.close();
+			}
+			object_in.close();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 }
